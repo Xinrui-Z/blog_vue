cac08f552710c06692a258d04823cbb91d207920
XinrZhou <1352492197@qq.com>
2023-05-04 21:50:56 +0800

    perf: 通过CDN节点引入库/框架资源

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,39 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path"
+import externalGlobals from 'rollup-plugin-external-globals'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve('./src') 
+      '@': path.resolve('./src')
     }
   },
-  server:{
+  server: {
     host: '0.0.0.0',
     port: 8080,
-    proxy:{
-      '/api': {  
+    proxy: {
+      '/api': {
         // target: 'http://localhost:8855/api',
         target: 'http://122.9.33.79:8855/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['vue', 'element-plus', 'pinia', 'vue-demi'],
+      plugins: [
+        externalGlobals({
+          vue: 'Vue',
+          'element-plus': 'ElementPlus',
+          pinia: 'Pinia',
+          'vue-router': 'VueRouter',
+          "vue-demi": "VueDemi"
+        }),
+      ],
     }
   }
 })
